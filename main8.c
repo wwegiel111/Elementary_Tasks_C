772744d32aae845abf2e598433b1989d8229f0c5
wwegiel <wwegiel@icloud.com>
2025-03-16 14:34:42 +0100

    8 taks

--- main8.c
+++ main8.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
+#include <math.h>
 
+int main(void) {
+    int n = 1;
+    while (pow(1.02, n) <= 1000.0 * n * n) {
+        n++;
+    }
+    printf("Part (a): n = %d\n", n);
+
+    double sum = 0.0;
+    n = 1;
+    while (sum <= M_E) {
+        sum += 1.0 / n;
+        n++;
+    }
+
+    printf("Part (b): n = %d\n", n - 1);
+
+    return 0;
+}
